20e47b4395ca73eb9ce3df0b981936d1d0bbac03
Elliott Rogers <elliott.e.w.rogers@gmail.com>
2024-01-30 16:34:44 +0000

    Created the scriptable object to hold the questions and answers for the game Created 5 questions

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -5,5 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
 public class QuestionSO : ScriptableObject 
 {
-    
+    [SerializeField] [TextArea (2,6)] string question = "Enter new question text here";
+    public string Question { get { return question; } }
+
+    [SerializeField] string[] answers = new string[4];
+
+    [SerializeField] [Range(0,3)] int correctAnswerNumber;
+    public int CorrectAnswerNumber { get { return correctAnswerNumber; } }
+
+    public string GetAnswer(int answerNumber)
+    {
+        return answers[answerNumber];
+    }
 }
