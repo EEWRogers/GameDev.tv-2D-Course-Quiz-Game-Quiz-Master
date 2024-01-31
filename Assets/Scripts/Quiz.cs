9b5a5466b7e79446c720e83601dd1ca2b38e7662
Elliott Rogers <elliott.e.w.rogers@gmail.com>
2024-01-31 12:37:13 +0000

    Added functionality to click on the answer buttons and either receive congratulations or a message telling you which answer was correct

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Quiz : MonoBehaviour
@@ -8,6 +9,9 @@ public class Quiz : MonoBehaviour
 [SerializeField] QuestionSO quizQuestion;
 [SerializeField] TextMeshProUGUI questionText;
 [SerializeField] GameObject[] answerButtons;
+[SerializeField] Sprite defaultAnswerSprite;
+[SerializeField] Sprite correctAnswerSprite;
+int correctAnswerNumber;
 
 void Start()
     {
@@ -23,6 +27,23 @@ void Start()
         {
             TextMeshProUGUI buttonText = answerButtons[answerNumber].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = quizQuestion.GetAnswer(answerNumber);
+        }
+    }
+
+    public void OnAnswerSelected(int answerNumber)
+    {
+        Image buttonImage = answerButtons[quizQuestion.CorrectAnswerNumber].GetComponent<Image>();
+        buttonImage.sprite = correctAnswerSprite;
+        correctAnswerNumber = quizQuestion.CorrectAnswerNumber;
+
+        if(answerNumber == quizQuestion.CorrectAnswerNumber)
+        {
+            questionText.text = "Correct!";
+        }
+        else
+        {
+            string correctAnswer = quizQuestion.GetAnswer(correctAnswerNumber);
+            questionText.text = "Incorrect! The correct answer was: \n" + correctAnswer;
         }
     }
 }
